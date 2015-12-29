e42d63b3b87879e9eeb97ebac039c49e483fa375
Petr Zvoníček <zvonicek@gmail.com>
2015-12-29 19:05:40 +0100

    ScaleTaskView implementation

--- EverydayMath/ScaleControl.swift
+++ EverydayMath/ScaleControl.swift
@@ -16,9 +16,12 @@ enum LabelPosition {
 
     var minValue: CGFloat = 10.5
     var maxValue: CGFloat = 22.4
-    var labelPosition: LabelPosition = .Bottom
     
+    var labelPosition: LabelPosition = .Bottom
     let contentInsets = UIEdgeInsetsMake(20, 20, 20, 20)
+    
+    private var contentRect: CGRect = CGRectZero
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -32,34 +35,85 @@ enum LabelPosition {
     }
     
     func initialize() {
-        self.backgroundColor = UIColor.whiteColor()
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
         
-    func labelPositions(rect: CGRect, range: Int) -> (index: Int) -> Bool {
-        let labelWidth = CGFloat(20)
+        contentRect = UIEdgeInsetsInsetRect(self.bounds, UIEdgeInsetsMake(self.bounds.height * 0.2, 10, self.bounds.height * 0.4, 10))
+    }
+    
+    func valueForPoint(point: CGPoint) -> CGFloat {
+        let positionInRange = (point.x - contentRect.origin.x) / contentRect.size.width
+        return positionInRange * (maxValue - minValue) + minValue
+    }
+    
+    func pointForValue(value: CGFloat) -> CGPoint {
+        if value < minValue || value > maxValue {
+            return CGPointZero
+        }
+        
+        let positionInRange = (value - minValue) / (maxValue - minValue)
+        return CGPointMake(positionInRange * contentRect.size.width + contentRect.origin.x, CGRectGetMidY(contentRect))
+    }
+    
+    // MARK: drawing methods
+        
+    private func labelPositions(rect: CGRect, labelWidth: CGFloat, range: Int) -> (index: Int) -> Bool {
         let minRange = CGFloat(range) * labelWidth / CGRectGetWidth(rect)
         
-        var value: Int
-        switch minRange {
-        case 0...1:
-            value = 1
-        case 1...10:
-            value = 10
-        case 10...100:
-            value = 100
-        default:
-            value = 0
+        var value: Int = Int(pow(10, ceil(log10(minRange)) as CGFloat) as CGFloat)
+        if (value / 5 > Int(minRange)) {
+            value = value / 2
+        } else if (value / 5 > Int(minRange)) {
+            value = value / 5
         }
         
         return { $0 % value == 0 }
+    }
+    
+    private static func trailingZerosCount(var number: Int) -> Int {
+        if (number == 0) {
+            return 0
+        }
+        
+        var counter = 0
+        while number % 10 == 0 {
+            counter++
+            number /= 10
+        }
+        
+        return counter
+    }
+    
+    private func scalePoints(startValue: Int, endValue: Int) -> (index: Int) -> (lineHeight: CGFloat, fontSize: Int)? {
+        var maxLevel: Int = 0
+        for i in startValue...endValue {
+            let level = Int(pow(Float(10), Float(ScaleControl.trailingZerosCount(i))))
+            if level > maxLevel {
+                maxLevel = level
+            }
+        }
+        
+        return {index in
+            if index % maxLevel == 0 {
+                return (lineHeight: CGRectGetHeight(self.contentRect), fontSize: 16)
+            } else if index % (maxLevel / 10) == 0 {
+                return (lineHeight: CGRectGetHeight(self.contentRect) * 0.4, fontSize: 12)
+            } else {
+                return nil
+            }
+        }
     }
     
     override func drawRect(rect: CGRect) {
         let startValue = Int(minValue * 10)
         let endValue = Int(maxValue * 10)
         let intervalSize = endValue - startValue
-        let scaleRect = UIEdgeInsetsInsetRect(rect, UIEdgeInsetsMake(rect.height * 0.2, 10, rect.height * 0.4, 10))
-        let labelPositions = self.labelPositions(scaleRect, range: intervalSize)
+        
+        let labelSize = NSString(format: "%d", Int(maxValue)).sizeWithAttributes([NSFontAttributeName: UIFont.systemFontOfSize(16)])
+        let labelPositions = self.labelPositions(contentRect, labelWidth: labelSize.width, range: intervalSize)
+        let scalePoints = self.scalePoints(startValue, endValue: endValue)
         
         let context = UIGraphicsGetCurrentContext()
         CGContextSetLineWidth(context, 2.0)
@@ -68,36 +122,18 @@ enum LabelPosition {
             UIColor.whiteColor().setStroke()
             UIColor.whiteColor().setFill()
             
-            let xPosition = CGRectGetMinX(scaleRect) + CGRectGetWidth(scaleRect) * CGFloat(Float(i - startValue) / Float(intervalSize))
+            let xPosition = CGRectGetMinX(contentRect) + CGRectGetWidth(contentRect) * CGFloat(Float(i - startValue) / Float(intervalSize))
             
-            var lineHeight: CGFloat?
-            var fontSize: Int = 0
-            
-            if i % 100 == 0 {
-                // print '10' line
-                lineHeight = CGRectGetHeight(scaleRect)
-                fontSize = 16
-            } else if i % 10 == 0 {
-                // print '1' line
-                lineHeight = CGRectGetHeight(scaleRect) * 0.4
-                fontSize = 12
-            } else {
-                // print '0.1' line
-                if (intervalSize < 100) {
+            if let point = scalePoints(index: i) {
+                CGContextMoveToPoint(context, xPosition, CGRectGetMinY(contentRect) + (CGRectGetHeight(contentRect) - point.lineHeight) / 2)
+                CGContextAddLineToPoint(context, xPosition, CGRectGetMinY(contentRect) + (CGRectGetHeight(contentRect) - point.lineHeight) / 2 + point.lineHeight)
+                CGContextStrokePath(context)
+
+                if labelPositions(index: i) {
+                    let number = i / 10
+                    drawLabel(contentRect, xPosition: xPosition, i: number, fontSize: CGFloat(point.fontSize))
                 }
             }
-
-            if let lineHeight = lineHeight {
-                CGContextMoveToPoint(context, xPosition, CGRectGetMinY(scaleRect) + (CGRectGetHeight(scaleRect) - lineHeight) / 2)
-                CGContextAddLineToPoint(context, xPosition, CGRectGetMinY(scaleRect) + (CGRectGetHeight(scaleRect) - lineHeight) / 2 + lineHeight)
-                CGContextStrokePath(context)
-            }
-            
-            if labelPositions(index: i) {
-                let number = i / 10
-                drawLabel(scaleRect, xPosition: xPosition, i: number, fontSize: CGFloat(fontSize))
-            }
-            
         }
     }
     
@@ -114,9 +150,80 @@ enum LabelPosition {
             NSParagraphStyleAttributeName: paragraphStyle
         ]
         
-        let labelSize = CGFloat(20)
-        let topPadding = labelSize / 2 - font.lineHeight / 2
-        label.drawInRect(CGRectMake(xPosition - labelSize / 2, CGRectGetMaxY(scaleRect) + labelSize / 2 + topPadding, labelSize, labelSize), withAttributes: attributes)
+        let labelSize = label.sizeWithAttributes([NSFontAttributeName: UIFont.systemFontOfSize(16)])
         
+        let topPadding = labelSize.height / 2 - font.lineHeight / 2
+        label.drawInRect(CGRectMake(xPosition - labelSize.width / 2, CGRectGetMaxY(scaleRect) + labelSize.height / 2 + topPadding, labelSize.width, labelSize.height), withAttributes: attributes)
+        
+    }
+}
+
+protocol TrackingScaleControlDelegate: class {
+    func scaleControlDidAnswer(value: CGFloat)
+}
+
+class TrackingScaleControl: ScaleControl {
+    
+    var correctTolerance: CGFloat = 1.0
+    var correctValue: CGFloat = 15.5
+    
+    weak var delegate: TrackingScaleControlDelegate?
+    
+    private let positionView: UIView = {
+        let view = UIView(frame: CGRectMake(0, 0, 20, 20))
+        view.backgroundColor = UIColor.whiteColor()
+        view.alpha = 0.5
+        view.hidden = true
+        view.userInteractionEnabled = false
+        return view
+    }()
+    
+    
+    override func initialize() {
+        super.initialize()
+        
+        self.addSubview(positionView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // set appropriate position view frame
+        let positionViewWidth = CGRectGetWidth(contentRect) * CGFloat(correctTolerance * 2 / (maxValue - minValue))
+        positionView.frame = CGRectMake(positionView.frame.origin.x, contentRect.origin.y, positionViewWidth, contentRect.size.height)
+    }
+
+    // MARK: tracking handling
+    
+    override func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+        positionView.hidden = false
+        setViewForTouch(touch)
+        return true
+    }
+    
+    override func continueTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+        setViewForTouch(touch)
+        return true
+    }
+    
+    override func endTrackingWithTouch(touch: UITouch?, withEvent event: UIEvent?) {
+        self.userInteractionEnabled = false
+        
+        let resultView = UIView(frame: CGRectMake(-10, CGRectGetMidY(contentRect) - 10, 10, 10))
+        resultView.backgroundColor = UIColor.greenColor()
+        resultView.alpha = 0.5
+        self.addSubview(resultView)
+        
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            resultView.center = self.pointForValue(self.correctValue)
+            }) { (finished) -> Void in
+            self.delegate?.scaleControlDidAnswer(self.valueForPoint(self.positionView.center))
+        }
+    }
+    
+    private func setViewForTouch(touch: UITouch) {
+        let point = touch.locationInView(self)
+        let xPosition = max(contentRect.origin.x, min(CGRectGetMaxX(contentRect), point.x))
+        positionView.center = CGPointMake(xPosition, positionView.center.y)
     }
 }
--- EverydayMath/ScaleTaskView.swift
+++ EverydayMath/ScaleTaskView.swift
@@ -8,14 +8,54 @@
 
 import UIKit
 
-class ScaleTaskView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+class ScaleTaskView: UIView, TrackingScaleControlDelegate {
+    
+    var delegate: TaskDelegate?
+    var task: ScaleTask! {
+        didSet {
+            self.taskLabel.text = task.configuration.task
+            
+            self.scaleControl.minValue = CGFloat(task.configuration.scaleMin)
+            self.scaleControl.maxValue = CGFloat(task.configuration.scaleMax)
+            self.scaleControl.correctValue = CGFloat(task.configuration.correctValue)
+            self.scaleControl.correctTolerance = CGFloat(task.configuration.correctTolerance)
+        }
     }
-    */
+    
+    @IBOutlet var taskLabel: UILabel!
+    @IBOutlet var scaleControl: TrackingScaleControl!
+    @IBOutlet var resultView: UIView!
+    @IBOutlet var resultTextLabel: UILabel!
+    @IBOutlet var resultNumberLabel: UILabel!
+    
+    override func awakeFromNib() {
+        self.backgroundColor = UIColor.clearColor()
+        scaleControl.delegate = self
+        resultView.alpha = 0.0
+    }
+    
+    // MARK: TrackingScaleControlDelegate
+    
+    func scaleControlDidAnswer(value: CGFloat) {
+        let isCorrect = Float(value) > task.configuration.correctValue - task.configuration.correctTolerance && Float(value) < task.configuration.correctValue + task.configuration.correctTolerance
+        
+        if  isCorrect {
+            resultTextLabel.text = "correct!"
+            resultTextLabel.textColor = UIColor.greenColor()
+        } else {
+            resultTextLabel.text = "incorrect!"
+            resultTextLabel.textColor = UIColor.redColor()
+        }
+        resultNumberLabel.text = String(format: "%.2f", task.configuration.correctValue)
 
+        
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.resultView.alpha = 1.0
+        }) { (let finished) -> Void in
+            let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
+            dispatch_after(delayTime, dispatch_get_main_queue()) {
+                self.delegate?.taskCompleted(self.task)
+            }
+        }
+    }
 }
