d9117f7dd8699c5c065db6e4d92706b07d8d8253
Petr Zvoníček <zvonicek@gmail.com>
2016-03-03 00:11:55 +0100

    HintConfiguration

--- EverydayMath/HintConfiguration.swift
+++ EverydayMath/HintConfiguration.swift
@@ -9,9 +9,42 @@
 import Foundation
 import Unbox
 
-protocol HintConfiguration {
+protocol HintConfiguration: Unboxable {
+    init(unboxer: Unboxer)
     func getHintView() -> UIView
 }
+
+enum HintType: String {
+    case Scale = "hintScale"
+    case Text = "hintText"
+    
+    static func unbox(dict: UnboxableDictionary) -> HintConfiguration? {
+        guard let typeString = dict["type"] as? String, let type = HintType(rawValue: typeString)
+            else { return nil }
+        
+        switch type {
+        case .Scale:
+            let q: ScaleHintConfiguration? = Unbox(dict)
+            return q
+        case .Text:
+            let q: TextHintConfiguration? = Unbox(dict)
+            return q
+        }
+    }
+}
+
+extension Unboxer {
+    func unbox(key: String) -> [HintConfiguration] {
+        guard let questions = self.dictionary[key] as? [UnboxableDictionary]
+            else { return [] }
+        
+        return questions.flatMap({ (dict) -> HintConfiguration? in
+            return HintType.unbox(dict)
+        })
+    }
+}
+
+// MARK: hints
 
 struct ScaleHintConfiguration: HintConfiguration {
     var topUnit: String
@@ -21,6 +54,15 @@ struct ScaleHintConfiguration: HintConfiguration {
     var bottomUnit: String
     var bottomMin: Float
     var bottomMax: Float
+    
+    init(unboxer: Unboxer) {
+        topUnit = unboxer.unbox("topUnit")
+        topMin = unboxer.unbox("topMin")
+        topMax = unboxer.unbox("topMax")
+        bottomUnit = unboxer.unbox("bottomUnit")
+        bottomMin = unboxer.unbox("bottomMin")
+        bottomMax = unboxer.unbox("bottomMax")
+    }
     
     func getHintView() -> UIView {
         let view = ScaleHintView.instanceFromNib()
@@ -35,8 +77,12 @@ struct ScaleHintConfiguration: HintConfiguration {
     }
 }
 
-struct NumericHintConfiguration: HintConfiguration {
+struct TextHintConfiguration: HintConfiguration {
     var text: String
+    
+    init(unboxer: Unboxer) {
+        text = unboxer.unbox("text")
+    }
     
     func getHintView() -> UIView {
         let view = NumericHintView.instanceFromNib()
