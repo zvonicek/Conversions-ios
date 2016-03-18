6ac0c3ae7c64622e0df79a54a98de3bb990c544d
Petr Zvoníček <zvonicek@gmail.com>
2016-03-18 14:23:51 +0100

    Added few result messages

--- EverydayMath/TaskRunResult.swift
+++ EverydayMath/TaskRunResult.swift
@@ -24,9 +24,17 @@ enum TaskRunResult {
     }
     
     func message() -> taskCompletedMessage {
+        print(self)
+        
         switch self {
-        case .Result(0.5...1, _, _, _):
-            return ("a", "b")
+        case .Result(_, 0...0.5, 0.5...1, _):
+            return ("Fast, but unprecise", "Great speed, try to work on your precision")
+        case .Result(0...0.3, 0...0.3, _, _):
+            return ("Perfect", "Great speed and corectness")
+        case .Result(0.1...0.4, _, _, _):
+            return ("Good job", "You did well")
+        case .Result(0.7...1, _, _, _):
+            return ("Don't give up", "Try it again")
         case .Result(0, _, _, _):
             return ("Congratulations!", "You've successfully completed all questions")
         default:
