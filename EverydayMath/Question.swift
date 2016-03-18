75745fe231487905170f8e61f3d97b37d761b39b
Petr Zvoníček <zvonicek@gmail.com>
2016-03-18 12:07:10 +0100

    Refactoring

--- EverydayMath/Question.swift
+++ EverydayMath/Question.swift
@@ -8,57 +8,6 @@
 
 import UIKit
 
-enum QuestionResultAccuracy {
-    case Imprecise, Precise, NonApplicable
-}
-
-enum QuestionResultSpeed {
-    case Fast, Slow, NonApplicable
-}
-
-enum QuestionResult {
-    case Incorrect, Correct(QuestionResultAccuracy, QuestionResultSpeed)
-    
-    func progressViewState() -> ProgressViewState {
-        switch self {
-        case .Incorrect:
-            return ProgressViewState.Incorrect
-        case .Correct(.Imprecise, .Fast), .Correct(.Precise, .Slow), .Correct(.NonApplicable, .Slow), .Correct(.Imprecise, .NonApplicable):
-            return ProgressViewState.CorrectB
-        case .Correct(.Imprecise, .Slow):
-            return ProgressViewState.CorrectC
-        case .Correct(.Precise, .Fast), .Correct(.NonApplicable, .Fast), .Correct(.Precise, .NonApplicable), .Correct(.NonApplicable, .NonApplicable):
-            return ProgressViewState.CorrectA
-        }
-    }
-    
-    func message() -> String {
-        switch self {
-        case .Incorrect:
-            return ["Oh, no!", "Maybe next time", "Bad luck", "That's not correct"].randomItem()
-        case .Correct(.Imprecise, .Fast), .Correct(.NonApplicable, .Fast):
-            return ["That was really fast", "Great speed", "You're really fast"].randomItem()
-        case .Correct(.Imprecise, .Slow), .Correct(.NonApplicable, .Slow), .Correct(.Imprecise, .NonApplicable), .Correct(.NonApplicable, .NonApplicable):
-            return ["Good effort", "Fair enough"].randomItem()
-        case .Correct(.Precise, .Fast):
-            return ["Fast and correct!", "Wow, quick and correct", "Quick and precise", "Great time and precision!"].randomItem()
-        case .Correct(.Precise, .Slow):
-            return ["Correct, try to be faster", "Correct, but not so fast"].randomItem()
-        case .Correct(.Precise, .NonApplicable):
-            return ["Precise!", "That's correct", "Great precision", "That's exactly it"].randomItem()
-        }
-    }
-    
-    func correct() -> Bool {
-        switch self {
-        case .Incorrect:
-            return false
-        default:
-            return true
-        }
-    }
-}
-
 protocol QuestionDelegate {
     func questionCompleted(question: Question, correct: Bool, accuracy: QuestionResultAccuracy?, answer: [String: AnyObject])
     func questionGaveSecondTry(question: Question)
---
+++ EverydayMath/TaskRunResult.swift
@@ -0,0 +1,36 @@
+//
+//  TaskRunResult.swift
+//  EverydayMath
+//
+//  Created by Petr Zvoníček on 18.03.16.
+//  Copyright © 2016 Petr Zvonicek. All rights reserved.
+//
+
+import Foundation
+
+typealias taskCompletedMessage = (title: String, subtitle: String)
+enum TaskRunResult {
+    case Result(incorrectRatio: Float, slowRatio: Float, impreciseRatio: Float, preciseRatio: Float)
+    
+    static func createFromRunLog(runLog: [QuestionRunLog]) -> TaskRunResult {
+        let correctQuestions = runLog.filter { $0.result.correct() }
+        
+        let incorrectRatio = 1 - (Float(correctQuestions.count) / Float(runLog.count))
+        let slowRatio = Float(correctQuestions.filter { if case .Correct(_, .Slow) = $0.result { return true } else { return false }}.count) / Float(correctQuestions.count)
+        let impreciseRatio = Float(correctQuestions.filter { if case .Correct(.Imprecise, _) = $0.result { return true } else { return false }}.count) / Float(correctQuestions.count)
+        let preciseRatio = Float(correctQuestions.filter { if case .Correct(.Precise, _) = $0.result { return true } else { return false }}.count) / Float(correctQuestions.count)
+        
+        return .Result(incorrectRatio: incorrectRatio, slowRatio: slowRatio, impreciseRatio: impreciseRatio, preciseRatio: preciseRatio)
+    }
+    
+    func message() -> taskCompletedMessage {
+        switch self {
+        case .Result(0.5...1, _, _, _):
+            return ("a", "b")
+        case .Result(0, _, _, _):
+            return ("Congratulations!", "You've successfully completed all questions")
+        default:
+            return ("Well done", "You're on the right track")
+        }
+    }
+}
