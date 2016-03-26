d21977ed212c63bc45b6c764011e9ae3e030ddf2
Petr Zvoníček <zvonicek@gmail.com>
2016-03-26 18:24:04 +0100

    Number formatter fix

--- EverydayMath/NumberFormatter.swift
+++ EverydayMath/NumberFormatter.swift
@@ -11,6 +11,7 @@ import Foundation
 extension NSNumberFormatter {
     @nonobjc public static let formatter: NSNumberFormatter = {
         let formatter = NSNumberFormatter()
+        formatter.minimumIntegerDigits = 1
         formatter.minimumFractionDigits = 0
         formatter.maximumFractionDigits = 3
         return formatter
